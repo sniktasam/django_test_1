a174e0f98319c418ae6d7c0396dd1a067c88880c
Sam Atkins <sniktasam@gmail.com>
2015-05-21 14:17:20 -0600

    third commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-     url(r'^testapp1/', include('testapp1.urls')),
+    url(r'^$', 'mysite.views.home', name='home'),
+    url(r'^testapp1/', include('testapp1.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
